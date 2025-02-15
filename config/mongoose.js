80fc0f8b616103cb4e019d9f159f1439031e7e97
dnsubakhyt <https://github.com/me50/dnsubkhyt>
2025-02-15 17:41:01 +0500

    removeed files

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
-mongoose.connect('mongodb://localhost:27017/books')
-.then(() => console.log('Connected to MongoDB'))
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log(`Connected to MongoDB`))
 .catch((err) => console.error('MongoDB connection error', err))
 
 module.exports = mongoose
