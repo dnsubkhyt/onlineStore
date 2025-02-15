9b282d67be9059015ba95c79cdb95f8b230ac82d
dnsubakhyt <https://github.com/me50/dnsubkhyt>
2025-02-15 19:55:03 +0500

    update git

--- routes/weatherRoutes.js
+++ routes/weatherRoutes.js
@@ -3,5 +3,4 @@ const router = express.Router()
 const weatherController = require('../controllers/weatherController')
 
 router.get('/:city', weatherController.getWeatherByCity)
-
 module.exports = router
