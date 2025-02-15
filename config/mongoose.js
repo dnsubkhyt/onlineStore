648a903b84d09fd17f3778bfeb9ca77ab42c0f00
thedracula <151816173+dnsubkhyt@users.noreply.github.com>
2025-02-15 16:17:46 +0500

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/books')
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('Connected to MongoDB'))
 .catch((err) => console.error('MongoDB connection error', err))
 
